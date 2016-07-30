87880a8a7400f63e98d6bc7994652b373f55b3a8
Zachary Lester <zachary.greg.lester@gmail.com>
2016-07-30 15:34:07 -0700

    Fix typo in jobs

--- jobs.js
+++ jobs.js
@@ -1,5 +1,5 @@
 const CronJob = require('cron').CronJob;
-const { updateServices } = reqiure('./workerUtils');
+const { updateServices } = require('./workerUtils');
 
 const updateServicesDaily = new CronJob({
   cronTime: '00 00 09 * * 0-6',
