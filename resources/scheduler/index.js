225d57cdfe433faf48394c4b8f189f9a2335d402
Zachary Lester <zachary.greg.lester@gmail.com>
2016-08-01 19:17:33 -0700

    Modify Scheduler jobs

--- resources/scheduler/index.js
+++ resources/scheduler/index.js
@@ -1,31 +1,31 @@
 const CronJob = require('cron').CronJob;
-const { updateServices, getLastUpdateStatus } = require('../jobs/Job');
+const Job = require('../jobs/Job');
 
 // Pings worker every 10 minutes to prevent Heroku from idling the dyno
 exports.pingWorkerEveryTenMinutes = new CronJob({
   cronTime: '00 */10 * * * 0-6',
   onTick: () => {
-    getLastUpdateStatus();
+    Job.getLastUpdateStatus();
   },
   start: true,
   timeZone: 'America/Los_Angeles',
 });
 
-// Updates all services daily at 7:30 a.m. PST
-exports.updateServicesDaily = new CronJob({
-  cronTime: '00 30 07 * * 0-6',
-  onTick: () => {
-    updateServices();
-  },
-  start: true,
-  timeZone: 'America/Los_Angeles',
-});
-
-// Updates all services Monday at the top of each hour
+// Updates all services at the top of each hour on Mondays
 exports.updateServicesMondayHourly = new CronJob({
   cronTime: '00 00 * * * 1',
   onTick: () => {
-    updateServices();
+    Job.updateServices();
+  },
+  start: true,
+  timeZone: 'America/Los_Angeles',
+});
+
+// Updates all services daily at 7:30 a.m. PST (in case of non-Monday rotations)
+exports.updateServicesDaily = new CronJob({
+  cronTime: '00 30 07 * * 0-6',
+  onTick: () => {
+    Job.updateServices();
   },
   start: true,
   timeZone: 'America/Los_Angeles',
